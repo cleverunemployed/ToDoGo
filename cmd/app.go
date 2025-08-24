5f7630cf95ffa189ba520971c4af35470c78a06e
cleverunemployed <agonycavarly@gmail.com>
2025-08-24 18:01:00 +0400

    bug fix

--- cmd/app.go
+++ cmd/app.go
@@ -1,27 +1,26 @@
 package main
 
-import "github/cleverunemployed/ToDoGo/test"
+// import "github/cleverunemployed/ToDoGo/test"
 
-// import (
-// 	handlers "github/cleverunemployed/ToDoGo/internal/api"
+import (
+	handlers "github/cleverunemployed/ToDoGo/internal/api"
 
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	// r := gin.Default()
+	r := gin.Default()
 
-	// api := r.Group("/api")
-	// {
-	// 	v1 := api.Group("/v1")
-	// 	{
-	// 		v1.GET("/get_all_tasks", handlers.GetAllTasks)
-	// 		v1.GET("/get_task/:id", handlers.GetTask)
-	// 		v1.POST("/add_task", handlers.AddTask)
-	// 		v1.DELETE("/delete_task", handlers.DeleteTask)
-	// 		v1.PATCH("/update_task", handlers.UpdateTask)
-	// 	}
-	// }
-	// r.Run()
-	test.TestMain()
+	api := r.Group("/api")
+	{
+		v1 := api.Group("/v1")
+		{
+			v1.GET("/get_all_tasks", handlers.GetAllTasks)
+			v1.GET("/get_task/:id", handlers.GetTask)
+			v1.POST("/add_task", handlers.AddTask)
+			v1.DELETE("/delete_task", handlers.DeleteTask)
+			v1.PATCH("/update_task", handlers.UpdateTask)
+		}
+	}
+	r.Run()
 }
